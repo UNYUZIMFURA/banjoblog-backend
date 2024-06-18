9fcda99074778d76b965491b4056dadecaffb716
UNYUZIMFURA <banjoflylive@gmail.com>
2024-06-18 12:58:23 +0200

    Feat: Add Swagger Documentation

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,136 @@
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - name
+ *         - author
+ *         - publisher
+ *         - publicationYear
+ *         - subject
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: The name of the book
+ *         author:
+ *           type: string
+ *           description: The author of the book
+ *         publisher:
+ *           type: string
+ *           description: The publisher of the book
+ *         publicationYear:
+ *           type: integer
+ *           description: The year the book was published
+ *         subject:
+ *           type: string
+ *           description: The subject of the book
+ *     User:
+ *       type: object
+ *       required:
+ *         - firstName
+ *         - lastName
+ *         - email
+ *         - password
+ *       properties:
+ *         firstName:
+ *           type: string
+ *           description: The first name of the user
+ *         lastName:
+ *           type: string
+ *           description: The last name of the user
+ *         email:
+ *           type: string
+ *           description: The email of the user
+ *         password:
+ *           type: string
+ *           description: The password of the user
+ */
+
+/**
+ * @swagger
+ * /api/v1/books:
+ *   get:
+ *     summary: Retrieve a list of books
+ *     responses:
+ *       200:
+ *         description: A list of books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
+ *   post:
+ *     summary: Create a new book
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Book'
+ *     responses:
+ *       200:
+ *         description: The created book
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Book'
+ */
+
+/**
+ * @swagger
+ * /auth/users:
+ *   post:
+ *     summary: Create a new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The created user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: User creation error
+ * /auth/login:
+ *   post:
+ *     summary: Login a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: The email of the user
+ *               password:
+ *                 type: string
+ *                 description: The password of the user
+ *     responses:
+ *       200:
+ *         description: Login successful
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       400:
+ *         description: Login error
+ */
+
+
 const express = require("express")
 const { findAllBooks, saveBooks } = require("../controllers/books.controller")
 const {createUser, login} = require("../controllers/auth.controller")
