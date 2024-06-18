420cd22c0c56246100bd47330fd151bbebcb6f0b
UNYUZIMFURA <banjoflylive@gmail.com>
2024-06-18 11:14:55 +0200

    Feat: Login API working

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const { findAllBooks, saveBooks } = require("../controllers/books.controller")
-const {createUser} = require("../controllers/auth.controller")
+const {createUser, login} = require("../controllers/auth.controller")
 const router = express.Router()
 
 router.get("/books", findAllBooks)
 router.post("/books", saveBooks)
 router.post("/auth/users", createUser)
-// router.post("/auth/")
+router.post("/auth/login", login)
 
 
 module.exports = router
